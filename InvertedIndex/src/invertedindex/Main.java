abbf5ec14d83d220c3396e5028bb6d42c87bb7a8
Kevin Duraj <kevinduraj@gmail.com>
2014-04-30 22:39:05 -0700

    mapper and reducer

---
+++ InvertedIndex/src/invertedindex/Main.java
@@ -0,0 +1,21 @@
+
+package invertedindex;
+
+import invertedindex.LineIndexer.LineIndexMapper;
+import java.io.IOException;
+import org.apache.hadoop.mapred.Reporter;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        LineIndexMapper mapper = new LineIndexMapper();
+        mapper.map(null, null, null, Reporter.NULL);
+        
+        LineIndexReducer reducer = new LineIndexReducer();
+        reducer.reduce(null, null, null, Reporter.NULL);
+
+    }
+    
+}
